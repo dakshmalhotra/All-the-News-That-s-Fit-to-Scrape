b3c0b7f652602238858ff28a7d36beb74e8a4b11
Roverkim <ryankim@hotmail.com>
2018-01-19 13:58:29 -0600

    Major Updates To Functionality. Add Materialize, MongoDB Support, Save Articles and Notes Support

--- controllers/webScrapperController.js
+++ controllers/webScrapperController.js
@@ -6,11 +6,10 @@ let db = require("../models"); // Require all models
 
 /////////////////////////////////////////////// /* Mongoose Configuration */ ////////////////////////////////////////////////////////
 mongoose.Promise = Promise; // Set mongoose to leverage Built in JavaScript ES6 Promises
-mongoose.connect("mongodb://localhost/webScrapper", { // Connect to the Mongo DB
+mongoose.connect("mongodb://heroku_n498q09l:nqhsgor6hvbhfudh35mk0npfo0@ds147267.mlab.com:47267/heroku_n498q09l", { // Connect to the Mongo DB
   useMongoClient: true
 });
 
-// mongodb://heroku_n498q09l:nqhsgor6hvbhfudh35mk0npfo0@ds147267.mlab.com:47267/heroku_n498q09l
 
 let mongooseConnection = mongoose.connection;
 
